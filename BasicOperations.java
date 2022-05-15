b85fc5cc0bc469ecd6622fb0f861d8d913f028e2
flizzyflo <vip3r1@gmx.de>
2022-05-15 16:14:49 +0200

    Upload basicMath

---
+++ BasicOperations.java
@@ -0,0 +1,27 @@
+public class BasicOperations 
+{
+    public static Integer basicMath(String op, int v1, int v2)
+    {
+        if (op.equals("+"))
+        {
+            return v1 + v2;
+        }
+        else if(op.equals("-"))
+        {
+            return v1 - v2;
+        }
+        else if (op.equals("*"))
+        {
+            return (int) v1 * v2;
+        }
+        else if (op.equals("/"))
+        {
+            return (int) v1 / v2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+}
