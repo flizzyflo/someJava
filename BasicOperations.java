2424a7d7efdd0f8035c52e58a0b0c1bcc7831796
flizzyflo <vip3r1@gmx.de>
2022-05-15 16:18:37 +0200

    Update BasicMath

--- BasicOperations.java
+++ BasicOperations.java
@@ -1,27 +1,34 @@
+import java.io.IOException;
+
 public class BasicOperations 
 {
-    public static Integer basicMath(String op, int v1, int v2)
+    public static Integer basicMath(String op, int v1, int v2) throws IllegalArgumentException
     {
-        if (op.equals("+"))
+        switch (op)
         {
-            return v1 + v2;
-        }
-        else if(op.equals("-"))
-        {
-            return v1 - v2;
-        }
-        else if (op.equals("*"))
-        {
-            return (int) v1 * v2;
-        }
-        else if (op.equals("/"))
-        {
-            return (int) v1 / v2;
-        }
-        else
-        {
+            case "+":
+            {
+                return v1 + v2;
+            }
+            case "-":
+            {
+                return v1 - v2;
+            }
+            case "*":
+            {
+                return (int) v1 * v2;
+            }
+            case "/":
+            {
+                if (v2 == 0)
+                {
+                    throw new IllegalArgumentException("Division by 0");
+                };
+                return (int) v1 / v2;
+            }
+            default:
             return 0;
+
         }
     }
-    
 }
