aec1ed05954d08586f48c09ca2146c33b800d3c6
flizzyflo <vip3r1@gmx.de>
2022-05-20 16:23:28 +0200

    upload volume Cuboid

---
+++ VolumeCoboid.java
@@ -0,0 +1,6 @@
+public class VolumeCoboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        
+        return length * width * height;
+      }
+}
