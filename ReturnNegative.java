e43ff8321299e98b7e25a06a41b0076ed9c8d9ed
flizzyflo <vip3r1@gmx.de>
2022-05-31 18:58:50 +0200

    return negative

---
+++ ReturnNegative.java
@@ -0,0 +1,14 @@
+public class ReturnNegative {
+    public static int makeNegative(final int x) {
+    
+        if (x <= 0)
+        {
+            return x;
+        }
+        
+        else 
+        {
+            return x * (-1);
+        }
+      }
+}
