6df9ea816946777165c69d1599198a976a8146dd
flizzyflo <vip3r1@gmx.de>
2022-05-31 16:01:37 +0200

    PositiveJavaCounter

---
+++ Positive.java
@@ -0,0 +1,17 @@
+public class Positive {
+    
+    public static int sum(int[] arr)
+    {
+        int sum = 0;
+
+        for (int number: arr)
+        {
+            if (number > 0)
+            {
+                sum += number;
+            }
+        }
+
+        return sum;
+    }
+}
