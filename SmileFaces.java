0763aff39156204fe05d3b1f73a70723ab17e55b
flizzyflo <vip3r1@gmx.de>
2022-05-31 20:33:53 +0200

    upload smileFaces

---
+++ SmileFaces.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class SmileFaces {
+  
+  public static int countSmileys(List<String> arr) {
+  {
+    List <String> items = Arrays.asList(":", ";", "D", ")");
+    int checkSum = 0;
+    int totalCount = 0;
+    
+    for (String smiley: arr)
+    {   
+
+       
+          if (smiley.equals(":D") ||smiley.equals(":)") || smiley.equals(":-D") || smiley.equals(":-)") || 
+          smiley.equals(":~)") || smiley.equals(":~D"))
+          {
+            checkSum ++;
+          };
+
+          if (smiley.equals(";D") ||smiley.equals(";)") || smiley.equals(";-D") || smiley.equals(";-)") || 
+          smiley.equals(";~)") || smiley.equals(";~D"))
+          {
+            checkSum ++;
+          };
+
+
+        
+
+        if (checkSum > 0)
+        {
+            totalCount ++;
+            checkSum = 0;
+        };
+    }
+
+    return totalCount;
+  }
+    
+}
+  
+}
