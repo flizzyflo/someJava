f1f198323c89ca3fe544a53e4a9a60d3cf78a8b9
flizzyflo <vip3r1@gmx.de>
2022-06-03 19:34:26 +0200

    Upload bubblesortOddNumbers

---
+++ BubblesortOddNumbers.java
@@ -0,0 +1,58 @@
+public class BubblesortOddNumbers {
+    public static int[] sortArray(int[] array) 
+    {   
+        int maxIteration = 0;
+        boolean sorted = false;
+        int counter = 1;
+
+        while (sorted == false)
+        {   
+            counter= 1;
+            
+            for (int i = 0; i < array.length;)
+            {   
+                if (array[i] % 2 == 0)
+                {
+                    i++;
+                    continue;
+                };
+                
+                if (i + counter > array.length - 1)
+                {
+                    break;
+                }
+
+                if ((array[i + counter] % 2 == 0))
+                {
+
+                    counter++;
+                    continue;
+                };
+                
+                if (array[i] > array[i + counter])
+                {
+                    int higherNumber;
+                    int lowerNumber;
+                    lowerNumber = array[i + counter];
+                    higherNumber = array[i];
+                    array[i + counter] = higherNumber;
+                    array[i] = lowerNumber;   
+                };
+
+                i ++;
+                counter = 1;
+
+
+            };
+
+            maxIteration ++;
+
+            if (maxIteration == array.length)
+            {
+                sorted = true;
+            };
+
+        }
+
+        return array;
+    }
