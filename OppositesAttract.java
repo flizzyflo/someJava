12e792178f406020bdb38ef47635057f97d844cb
flizzyflo <vip3r1@gmx.de>
2022-07-13 19:58:42 +0200

    upload of oppositesAttract

---
+++ OppositesAttract.java
@@ -0,0 +1,12 @@
+public class OppositesAttract {
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        
+        if (flower1 % 2 == 0 && flower2 % 2 == 0){
+          return false;
+        };
+      
+        return (flower1 % 2 == 0 || flower2 % 2 == 0);
+    };
+    
+  };
