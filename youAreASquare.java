ee7c68f09ca1bb8302128327b93f907bab477a64
flizzyflo <102415350+flizzyflo@users.noreply.github.com>
2022-07-19 09:18:01 +0200

    Create youAreASquare.java

---
+++ youAreASquare.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+public class Square {    
+    public static boolean isSquare(int n) {        
+        if (n == 0){
+          return true;
+        };
+        return Math.pow(n, 0.5) == Math.floor(Math.pow(n, 0.5));
+    };
+};
